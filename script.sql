6994af357fb05e6259869a38459f9159c2712f88
donxd <n0s3.xd@gmail.com>
2019-10-25 11:31:13 -0500

    changes script table

--- script.sql
+++ script.sql
@@ -6,5 +6,7 @@ create table secuences (
     id int not null auto_increment,
     dna text(500) not null,
     result tinyint(1) not null,
-    primary key (id)
+    primary key (id),
+    key secuences_result_IDX (result) using btree,
+    fulltext key secuences_dna_IDX (dna)
 ) engine=InnoDB charset=utf8;
